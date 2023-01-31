f769a7246e16e5504279ed1c75f172f72987da2e
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-01-31 14:05:41 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+if (value <= 400){
+  return "This one is on me!"
+}else if (value <= 2000){
+  return "That will be twenty bucks."
+}if (value <= 2500){
+  return "I will gladly take your thirty bucks."
+}if (value > 2500){
+  return "No can do."
+}
+}scuberGreetingForFeet()
+
+function ternaryCheckCity(city){
+  return city === `NYC`? `Ok, sounds good.`: `No go.`
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(trip){
+  switch (trip){
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
